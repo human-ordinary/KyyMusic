ead117ccd27a642f5e4ada81c0524c5cd9fd0feb
muk <76889639+human-ordinary@users.noreply.github.com>
2022-02-21 22:08:51 +0700

    Update config.py

--- Music/config.py
+++ Music/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 get_queue = {}
 SESSION_NAME = getenv('SESSION_NAME', 'session')
 BOT_TOKEN = getenv('BOT_TOKEN')
-API_ID = int(getenv('API_ID', "10892147"))
+API_ID = int(getenv('API_ID', "16317710"))
 API_HASH = getenv('API_HASH')
 DURATION_LIMIT = int(getenv('DURATION_LIMIT', '360000'))
 COMMAND_PREFIXES = list(getenv('COMMAND_PREFIXES', '/ . , : ; !').split())
